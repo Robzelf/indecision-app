11e66141beb25f301d7d4ffe37a6b7d4d4a3261f
Rob Peters <rob@frontdev.nl>
2024-02-21 23:01:04 +0100

    Lesson #51

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-console.log('runnin');
+// import { square, add } from  './utils.js';
+
+// console.log(square(4));
+// console.log(add(4,4));
+
+import { isAdult, canDrink } from  './person.js';
+console.log( isAdult(16) );
+console.log( canDrink(20) );
---
+++ src/person.js
@@ -0,0 +1,4 @@
+const isAdult = (_age) => _age >= 18;
+const canDrink = (_age) => _age >= 21;
+
+export { isAdult, canDrink };
